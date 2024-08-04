c83312a6cee378a07de64ab0d7d3655dae24d871
Mesut Suhan Şişman <127686800+Mesutssmn@users.noreply.github.com>
2024-08-04 18:09:39 +0300

    Update steam.py

--- steam.py
+++ steam.py
@@ -56,6 +56,9 @@ music_html = """
   <source src="https://soundfxcenter.com/video-games/counter-strike/8d82b5_Counter_Strike_AK_47_Bolt_Pull_Sound_Effect.mp3" type="audio/mpeg">
   Your browser does not support the audio element.
 </audio>
+<script>
+document.getElementById('background-music').volume = 0.2;
+</script>
 """
 
 st.markdown(music_html, unsafe_allow_html=True)
